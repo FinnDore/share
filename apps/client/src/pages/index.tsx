04f214aed0fffc8d291b78189806ee06c1dd5877
Finn <34718806+FinnDore@users.noreply.github.com>
2023-05-06 21:12:21 +0100

    added tauri

---
+++ apps/client/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-screen w-screen bg-black">
+      <div className="bold m-auto text-4xl text-rose-600">k</div>
+    </div>
+  );
+};
+
+export default Home;
