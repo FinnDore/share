73a04ba73fa3b25b8db02c7ef1abc01677c232d3
Finn <34718806+FinnDore@users.noreply.github.com>
2023-05-12 19:15:29 +0100

    .

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { MacTrafficLights } from "~/components/traffic-lights";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen w-screen rounded-lg border border-white/20 bg-black">
+    <div className="flex h-screen w-screen rounded-lg border border-white/25 bg-black">
       <div className="w-scrreen ps-2 pt-2" data-tauri-drag-region>
         <MacTrafficLights />
       </div>
